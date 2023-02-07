bcbaadaf34d561a4935ea8d651f0745147fe7dfc
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-07 17:55:16 -0300

    agregue la seccion sobre mi

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
-import { db, postProject } from '../API/api';
 import Swal from 'sweetalert2';
 import './NewProject.css';
+import axios from 'axios';
 
 /* INTERFACES Y ELEMENTOS DE TYPESCRIPT */
 
@@ -68,7 +68,10 @@ export const NewProject = () => {
     //Esta linea es para que no se recargue la pagina
     e.preventDefault();
     //funcion para agregar proyectos a la db
-    await postProject(db, project);
+    await axios.post(
+      'https://portfolio-back-production-ca39.up.railway.app/projects',
+      project
+    );
     //Este set sirve para limpiar los inputs cuando se postea un proyecto
     setProject({
       title: '',
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,13 @@ import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { ProjectsContainer } from '../components/ProjectsContainer';
 import './Home.css';
+import { About } from '../components/About';
 
 export const Home = () => {
   return (
     <div className='container-fluid home'>
       <Header />
+      <About />
       <ProjectsContainer />
       <Footer />
     </div>
