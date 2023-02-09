844828c64eeacd8f79b81466a6bd3082bdb2d84e
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-09 01:53:39 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import dns from 'dns';
-dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 
 export default ({ mode }) => {
@@ -11,9 +9,5 @@ export default ({ mode }) => {
   // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
   return defineConfig({
     plugins: [react()],
-    server: {
-      strictPort: false,
-      port: parseInt(process.env.PORT),
-    },
   });
 };
