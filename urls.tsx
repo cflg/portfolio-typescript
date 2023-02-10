b09b129691c0663472ae03646b45732a1cfc47fc
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-10 04:04:41 -0300

    s

--- urls.tsx
+++ urls.tsx
@@ -1,5 +1,4 @@
-/* export const back: string =
+export const back: string =
   'https://portfolio-back-production-ca39.up.railway.app';
- */
 
-export const back: string = 'http://localhost:3001';
+//export const back: string = 'http://localhost:3001';
