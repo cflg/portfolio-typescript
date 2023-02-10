c6b44cf5ae69559099777a097d265c07e8128fc5
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-10 14:15:34 -0300

    details

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import { Home } from './pages/Home';
 import './App.css';
 import { UserActions } from './pages/UserActions';
 import { ProjectDetails } from './components/ProjectDetails';
-import { PrDetails } from './components/PrDetails';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/admin' element={<UserActions />} />
-      <Route path='/details/:id' element={<PrDetails />} />
+      <Route path='/details/:id' element={<ProjectDetails />} />
     </Routes>
   );
 }
