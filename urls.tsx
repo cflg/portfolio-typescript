7a49504496e1e6cc18d34afd39b351466072f654
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-10 04:56:47 -0300

    eliminacion de favicon

--- urls.tsx
+++ urls.tsx
@@ -1,4 +1,4 @@
 export const back: string =
-  'https://portfolio-back-production-ca39.up.railway.app'; 
+  'https://portfolio-back-production-ca39.up.railway.app';
 
 //export const back: string = 'http://localhost:3001';
