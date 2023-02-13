c44a0639255e9328d072a4d34da1b1e5abb1187e
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-13 18:50:00 -0300

    a

--- src/components/NewComponent/New.tsx
+++ src/components/NewComponent/New.tsx
@@ -1,12 +1,4 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { back } from '../../../urls';
-import axios from 'axios';
-
 const New = () => {
-  let { id } = useParams();
-
-  console.log(id);
   return (
     <div>
       <p>Componente nuevo</p>
