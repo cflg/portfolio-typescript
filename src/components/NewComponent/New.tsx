c76bbfcde5a4ab4f9ef5e60f2d3a72982d367336
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-13 18:38:29 -0300

    r

--- src/components/NewComponent/New.tsx
+++ src/components/NewComponent/New.tsx
@@ -4,33 +4,21 @@ import { back } from '../../../urls';
 import axios from 'axios';
 
 const New = () => {
-  try {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [project, setProject] = useState<any>(null);
+  const [project, setProject] = useState<any>(null);
 
-    useEffect(() => {
-      if (id) {
-        axios.get(`${back}/projects/${id}`).then((res) => {
-          setProject(res.data);
-        });
-      }
-    }, [id]);
+  useEffect(() => {
+    fetch(`${back}/projects/${id}`, {
+      method: 'GET',
+    }).then((res) => setProject(res));
+  }, [id]);
 
-    return (
-      <div>
-        <p>Componente nuevo</p>
-        <p>{project && project.id}</p>
-      </div>
-    );
-  } catch (error) {
-    console.log(error);
-    return (
-      <div>
-        <p>Componente nuevo</p>
-        <p>Error</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Componente nuevo</p>
+      <p>{project && project.id}</p>
+    </div>
+  );
 };
 export default New;
