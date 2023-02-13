bd68a7ef1e365b40f7123d51123749e7bac28ce1
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-13 08:11:04 -0300

    css

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ProjectsDetails } from '../ProjectsDetails/ProjectsDetails';
 import { Header } from '../Header/Header';
-import './ProjectDetails.css';
+import '../ProjectDetails/ProjectDetails.css';
 import { Footer } from '../Footer/Footer';
 
 export const Projects = () => {
