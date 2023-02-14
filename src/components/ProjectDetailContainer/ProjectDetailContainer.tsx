6a45b37896849410be82a421f95c9e88e071c74f
Cristian Lo Giudice <cflg.dev@gmail.com>
2023-02-14 14:11:14 -0300

    user en todos los componentes

--- src/components/ProjectDetailContainer/ProjectDetailContainer.tsx
+++ src/components/ProjectDetailContainer/ProjectDetailContainer.tsx
@@ -3,11 +3,17 @@ import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import { ProjectsDetails } from '../ProjectsDetails/ProjectsDetails';
 import './ProjectDetailContainer.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const ProjectDetailContainer = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  let userEmail: string | undefined = '';
+  if (!isLoading && isAuthenticated && user) {
+    userEmail = user.email;
+  }
   return (
     <div className='container-fluid details-container'>
-      <Header />
+      <Header user={userEmail} />
       <Link to='/'>
         <button className='btn btn-outline-secondary btn-details'>
           ↵ Volver
